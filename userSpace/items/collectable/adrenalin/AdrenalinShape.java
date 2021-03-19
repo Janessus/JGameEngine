3a9e5b4c508204629db9f4769e0ab43b8100a546
Janessus <janesheuberger@web.de>
2021-03-19 10:31:10 +0100

    ImageNotFound properly handled with an alternative texture

--- userSpace/items/collectable/adrenalin/AdrenalinShape.java
+++ userSpace/items/collectable/adrenalin/AdrenalinShape.java
@@ -1,5 +1,8 @@
 package userSpace.items.collectable.adrenalin;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -18,10 +21,22 @@ public class AdrenalinShape extends GameShape
 		try {
 			texture = ImageIO.read(new File("./src/resources/adrenalin.png").getAbsoluteFile());
 		} catch (IOException e) {
-			e.printStackTrace(); //TODO
-//			texture = textureNotFound();
+			System.out.println("Width " + super.getSize().getWidth());
+			texture = new BufferedImage(super.getSize().width, super.getSize().height, BufferedImage.TYPE_INT_ARGB);
+			Graphics2D g = texture.createGraphics();
+			g.setColor(Color.BLACK);
+			g.setFont(new Font("monospace", Font.BOLD, 8));
+			drawMultiLineString(g, "   TEXTURE\nNOT FOUND", 6, 15);
+			g.dispose();
 		}
 	}
+	
+	
+	void drawMultiLineString(Graphics g, String text, int x, int y) {
+	    for (String line : text.split("\n"))
+	        g.drawString(line, x, y += g.getFontMetrics().getHeight());
+	}
+	
 
 	@Override
 	public void paint(Graphics g)
